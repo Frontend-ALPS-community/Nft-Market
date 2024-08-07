4e406460e24776b920c063a979671e732ab5b155
JAE HOON LEE <jazzyrain722@naver.com>
2024-08-07 12:01:59 +0900

    :lipstick:유틸리티바 스타일 수정.

--- src/@components/headers/header.tsx
+++ src/@components/headers/header.tsx
@@ -3,16 +3,22 @@ import Link from 'next/link';
 const Header = () => {
   return (
     <>
-      <div className="min-w-[380px] w-full h-[80px] bg-slate-400 flex justify-between  items-center px-[25px]">
+      <div className="min-w-[380px] w-full h-[80px] bg-slate-200 flex justify-between items-center px-[25px]">
         <Link href="/" className="flex items-center">
-          <img width={50} src="assets/logo/miniLogo.png"></img>
+          <img width={50} src="assets/logo/miniLogo.png" alt="Logo" />
           <div>서비스명</div>
         </Link>
-        <div className="flex  text-xs  sm:text-base ">
-          <Link href="/auth" className="mr-[10px] rounded-lg p-2 bg-orange-300">
+        <div className="flex text-xs sm:text-base">
+          <Link
+            href="/auth?type=login"
+            className="mr-[10px] rounded-lg p-2 bg-slate-300 hover:bg-slate-400"
+          >
             로그인
           </Link>
-          <Link href="/auth" className="mr-[30px] rounded-lg p-2 bg-orange-300">
+          <Link
+            href="/auth?type=signup"
+            className="mr-[30px] rounded-lg p-2 bg-slate-300 hover:bg-slate-400"
+          >
             회원가입
           </Link>
           <div>🟰</div>
--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,24 +1,39 @@
 'use client';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Login from './form/login';
 import Signup from './form/signup';
 
 const Page: React.FC = () => {
   const [isSignup, setIsSignup] = useState(false);
 
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      const params = new URLSearchParams(window.location.search);
+      //search 속성은 URL의 쿼리 문자열을 포함
+      const type = params.get('type');
+      setIsSignup(type === 'signup');
+    }
+  }, []);
+
   const toggleForm = () => {
     setIsSignup(!isSignup);
+    const newType = isSignup ? 'login' : 'signup';
+    window.history.pushState({}, '', `/auth?type=${newType}`);
   };
 
   return (
-    <div className="relative w-full h-[calc(100vh-8vh)] overflow-hidden flex justify-center items-center bg-slate-400">
+    <div className="relative w-full h-[calc(100vh-8vh)] overflow-hidden flex justify-center items-center bg-slate-200">
       <div
-        className={`w-[550px] h-[700px] transition-transform duration-500 transform ${isSignup ? 'translate-x-full' : ''} flex justify-center items-center bg-pink-50`}
+        className={`w-[550px] h-[700px] transition-transform duration-500 transform ${
+          isSignup ? 'translate-x-full' : ''
+        } flex justify-center items-center bg-pink-50`}
       >
         {isSignup ? <Signup /> : <Login />}
       </div>
       <div
-        className={`w-[550px] h-[700px] bg-blue-500 text-white flex justify-center items-center cursor-pointer transition-transform duration-500 transform  ${isSignup ? '-translate-x-full' : ''} `}
+        className={`w-[550px] h-[700px] bg-blue-500 text-white flex justify-center items-center cursor-pointer transition-transform duration-500 transform ${
+          isSignup ? '-translate-x-full' : ''
+        }`}
         onClick={toggleForm}
       >
         <div className="text-2xl">
