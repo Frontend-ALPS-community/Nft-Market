10b47fc7cd45bc7c5caa59f63c51e5cc8fa96eff
YoungSeungJang <1345620@naver.com>
2024-08-08 17:57:11 +0900

    authApi PATH 추가

--- src/apis/authApi.ts
+++ src/apis/authApi.ts
@@ -9,19 +9,25 @@ interface LoginPayload {
   email: string;
   password: string;
 }
+
+const PATH = '/auth';
 export const authApi = {
   async register({ username, email, password }: RegisterPayload) {
-    const res = await Axios.post('/register', { username, email, password });
+    const res = await Axios.post(PATH + '/register', {
+      username,
+      email,
+      password,
+    });
     return res.data;
   },
 
   async login({ email, password }: LoginPayload) {
-    const res = await Axios.post('/login', { email, password });
+    const res = await Axios.post(PATH + '/login', { email, password });
     return res.data;
   },
 
   async logout() {
-    const res = await Axios.post('/logout');
+    const res = await Axios.post(PATH + '/logout');
     return res.data;
   },
 };
