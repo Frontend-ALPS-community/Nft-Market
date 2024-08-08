d6c07f3a5a4437a42e4b0bd57c77f58b23155331
JAE HOON LEE <jazzyrain722@naver.com>
2024-08-09 00:45:04 +0900

    :memo: 로그인 쿠키(토큰)관련 로직 추가 및 디자인 수정.

--- src/@components/headers/header.tsx
+++ src/@components/headers/header.tsx
@@ -1,65 +1,68 @@
 'use client';
 import { authApi } from '@/apis/authApi';
-import Image from 'next/image';
 import Link from 'next/link';
+import { useEffect, useState } from 'react';
 
 const Header = () => {
+  const [tokenBool, setTokenBool] = useState(false);
+
   const onClickLogOutBtn = async () => {
     const res = await authApi.logout();
   };
+
+  useEffect(() => {
+    //쿠키 읽기.
+    const getCookie = (name: string): string | undefined => {
+      const value = `; ${document.cookie}`;
+      const parts = value.split(`; ${name}=`);
+      if (parts.length === 2) return parts.pop()?.split(';').shift();
+    };
+
+    const token = getCookie('token');
+    //console.log('Token stored in cookie:', token);
+    if (token) {
+      setTokenBool(true);
+      //console.log(tokenBool);
+    }
+  }, []);
+
   return (
     <>
-      <div className="min-w-[380px] w-full h-[80px] bg-slate-200 flex justify-between items-center px-[15px]">
+      <div className="min-w-[380px] w-full h-[80px] bg-gradient-to-r from-blue-500 to-purple-500 flex justify-between items-center px-[20px] shadow-lg">
         <Link href="/" className="flex items-center">
-          <img width={50} src="assets/logo/miniLogo.png" alt="Logo" />
-          <div>서비스명</div>
+          <img
+            width={50}
+            src="assets/logo/miniLogo.png"
+            alt="Logo"
+            className="mr-[10px]"
+          />
+          <div className="text-white font-semibold text-lg">Service Name</div>
         </Link>
         <div className="flex text-xs sm:text-base items-center">
-          <Link
-            href="/auth?type=login"
-            className="mr-[10px] h-10 rounded-lg p-2 bg-slate-300 hover:bg-slate-400"
-          >
-            로그인
-          </Link>
-          <Link
-            href="/auth?type=signup"
-            className="mr-[10px] h-10 rounded-lg p-2 bg-slate-300 hover:bg-slate-400"
-          >
-            회원가입
-          </Link>
-          <div className="relative group">
-            <div className="hover:-rotate-180 transition-transform duration-300">
-              <Image
-                width={40}
-                height={40}
-                src="/assets/logo/drop.png"
-                alt="Category Logo"
-              />
-            </div>
-            <div className="absolute right-0 mt-2 w-48 bg-white border border-gray-200 shadow-lg rounded-lg hidden group-hover:block ">
-              <div className="py-1">
-                <Link
-                  href="/category/1"
-                  className="block px-4 py-2 text-gray-700 hover:bg-gray-100"
-                >
-                  Category 1
-                </Link>
-                <Link
-                  href="/category/2"
-                  className="block px-4 py-2 text-gray-700 hover:bg-gray-100"
-                >
-                  Category 2
-                </Link>
-                <Link
-                  href="/category/3"
-                  className="block px-4 py-2 text-gray-700 hover:bg-gray-100"
-                >
-                  Category 3
-                </Link>
-              </div>
-            </div>
-          </div>
-          <button onClick={onClickLogOutBtn}>로그아웃</button>
+          {!tokenBool && (
+            <>
+              <Link
+                href="/auth?type=login"
+                className="mr-[10px] h-10 rounded-full px-4 bg-white text-blue-500 hover:bg-gray-100 transition duration-300 flex items-center justify-center"
+              >
+                로그인
+              </Link>
+              <Link
+                href="/auth?type=signup"
+                className="mr-[10px] h-10 rounded-full px-4 bg-white text-blue-500 hover:bg-gray-100 transition duration-300 flex items-center justify-center"
+              >
+                회원가입
+              </Link>
+            </>
+          )}
+          {tokenBool && (
+            <button
+              onClick={onClickLogOutBtn}
+              className="h-10 rounded-full px-4 bg-white text-blue-500 hover:bg-gray-100 transition duration-300 flex items-center justify-center"
+            >
+              로그아웃
+            </button>
+          )}
         </div>
       </div>
     </>
