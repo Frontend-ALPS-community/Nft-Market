fe3676c1adb10383629bba777ad7f07ac03cdf89
JAE HOON LEE <jazzyrain722@naver.com>
2024-08-09 19:44:14 +0900

    :lipstick: css수정 및 로직 추가.

--- src/app/auth/components/login.tsx
+++ src/app/auth/components/login.tsx
@@ -1,23 +1,26 @@
 import { authApi } from '@/apis/authApi';
+import { useRouter } from 'next/navigation';
 import React, { useState } from 'react';
 
 const Login: React.FC = () => {
   const [email, setEmail] = useState<string>('');
   const [password, setPassword] = useState<string>('');
+  const router = useRouter();
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
     try {
       const res = await authApi.login({ email, password });
-      console.log(res);
+      router.push('/');
     } catch (err) {
       console.log(err);
+      alert('로그인 실패: 이메일 또는 비밀번호를 확인하세요.');
     }
   };
 
   return (
-    <div>
+    <div className="centered-flex">
       <div className="w-full max-w-md p-4 sm:p-6">
         <form
           className="bg-white p-4 sm:p-6 rounded shadow-md relative"
@@ -42,7 +45,7 @@ const Login: React.FC = () => {
             />
           </div>
 
-          <div className="items-center">
+          <div className="centered-flex">
             <button
               type="submit"
               className="w-full px-4 py-2 bg-black text-white rounded-md"
--- src/app/auth/components/signup.tsx
+++ src/app/auth/components/signup.tsx
@@ -1,4 +1,5 @@
 import { authApi } from '@/apis/authApi';
+import { useRouter } from 'next/navigation';
 import React, { useState } from 'react';
 
 const Signup: React.FC = () => {
@@ -6,23 +7,22 @@ const Signup: React.FC = () => {
   const [password, setPassword] = useState<string>('');
   const [confirmPassword, setConfirmPassword] = useState<string>('');
   const [username, setUsername] = useState<string>('');
+  const router = useRouter();
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
-    // 유효성 검사 (hook-form, zod 등등)
-    // if (password !== confirmPassword) {
-    //   alert('비밀번호가 일치하지 않습니다!!!');
-    //   return;
-    // }
     try {
       const res = await authApi.register({
         username,
         email,
         password,
       });
+      router.push('/auth?type=login');
+      window.location.replace('/auth?type=login');
     } catch (err) {
       console.log(err);
+      alert('정보를 다시 확인해주세요');
     }
   };
 
@@ -40,12 +40,6 @@ const Signup: React.FC = () => {
             value={email}
             onChange={(e) => setEmail(e.target.value)}
           />
-          <button
-            type="button"
-            className="absolute top-0 right-0 px-3 py-1 bg-blue-500 hover:bg-blue-700 text-white rounded-md"
-          >
-            중복체크
-          </button>
         </div>
 
         <div className="mb-6 relative">
@@ -76,15 +70,9 @@ const Signup: React.FC = () => {
             value={username}
             onChange={(e) => setUsername(e.target.value)}
           />
-          <button
-            type="button"
-            className="absolute top-0 right-0 px-3 py-1 bg-blue-500 hover:bg-blue-700 text-white rounded-md"
-          >
-            중복체크
-          </button>
         </div>
 
-        <div className="items-center">
+        <div className="centered-flex">
           <button
             type="submit"
             className="w-full px-4 py-2 bg-black text-white rounded-md"
