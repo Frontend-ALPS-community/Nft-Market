d40ee5054eca9a07fbbeb4fcee0d30dcba03d577
JAE HOON LEE <jazzyrain722@naver.com>
2024-09-10 04:44:03 +0900

    :memo:제안 페이지 기능 완료.

--- src/app/mypage/[id]/offers/components/OfferList.tsx
+++ src/app/mypage/[id]/offers/components/OfferList.tsx
@@ -1,34 +1,63 @@
 'use client';
+import { CardApi } from '@/apis/cardApi';
 import Image from 'next/image';
 import Link from 'next/link';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import SortButtons from './SortBtn';
 
 export interface Offer {
-  id: number;
+  cardId: string;
   cardName: string;
-  offeredPrice: string;
+  price: string;
   date: string;
-  to: string; // 수신자
+  owner: string;
+  _id: string; // 수신자
 }
 
 interface OffersListProps {
   offers: Offer[];
-  //res: CardItem[];
 }
 
 const OffersList: React.FC<OffersListProps> = ({ offers }) => {
   const [priceAsc, setPriceAsc] = useState<boolean | null>(true);
   const [dateAsc, setDateAsc] = useState<boolean | null>(null);
+  const [cardsData, setCardsData] = useState<{ [key: string]: any }>({}); // cardId에 대한 카드 데이터를 저장하는 상태
+  console.log(offers);
+  useEffect(() => {
+    const fetchCardData = async (cardId: string) => {
+      try {
+        const cardData = await CardApi.getCard(cardId);
+        //console.log(cardData); // 카드 정보를 가져오는 API 호출
+        setCardsData((prevData) => ({
+          ...prevData,
+          [cardId]: cardData, // cardId에 대한 데이터 저장
+        }));
+        // console.log(cardData);
+      } catch (error) {
+        console.error(`Error fetching card with id ${cardId}:`, error);
+      }
+    };
+
+    // 각 offer에 대한 카드 데이터를 가져오기
+    offers.forEach((offer) => {
+      if (!cardsData[offer.cardId]) {
+        fetchCardData(offer.cardId); // 카드 데이터가 없는 경우에만 요청
+      }
+    });
+  }, [offers, cardsData]); // offers 또는 cardsData가 변경될 때마다 실행
 
   const sortedOffers = offers.slice().sort((a, b) => {
     if (priceAsc !== null) {
-      const priceA = parseFloat(a.offeredPrice.replace('$', ''));
-      const priceB = parseFloat(b.offeredPrice.replace('$', ''));
+      // price가 string일 경우 숫자로 변환
+      const priceA: number =
+        typeof a.price === 'string' ? parseFloat(a.price) : a.price;
+      const priceB: number =
+        typeof b.price === 'string' ? parseFloat(b.price) : b.price;
+
       return priceAsc ? priceA - priceB : priceB - priceA;
     } else {
-      const dateA = new Date(a.date).getTime();
-      const dateB = new Date(b.date).getTime();
+      const dateA: number = new Date(a.date).getTime(); // date는 number로 변환
+      const dateB: number = new Date(b.date).getTime(); // date는 number로 변환
       return dateAsc ? dateA - dateB : dateB - dateA;
     }
   });
@@ -49,23 +78,38 @@ const OffersList: React.FC<OffersListProps> = ({ offers }) => {
       />
       <div className="mt-6 space-y-4">
         {sortedOffers.map((offer) => (
-          <Link key={offer.id} href={`/assets/${offer.id}`} passHref>
+          <Link key={offer.cardId} href={`/assets/${offer.cardId}`} passHref>
             <div className="block p-4 bg-gray-50 hover:bg-gray-200 rounded-lg shadow-sm transition duration-150 ease-in-out mb-4">
               <div className="grid grid-cols-4 items-center">
-                <Image
-                  className="col-span-1"
-                  width={50}
-                  height={50}
-                  alt={offer.cardName}
-                  src="/path/to/default/image.jpg"
-                  unoptimized
-                />
-                <div className="col-span-1 text-center">{offer.cardName}</div>
+                {cardsData[offer.cardId] ? (
+                  <>
+                    {/* 카드 데이터가 있을 때 렌더링 */}
+                    <Image
+                      className="col-span-1"
+                      width={50}
+                      height={50}
+                      alt={cardsData[offer.cardId].cardName}
+                      src={
+                        process.env.NEXT_PUBLIC_Backend_URL
+                          ? process.env.NEXT_PUBLIC_Backend_URL +
+                            cardsData[offer.cardId].image
+                          : '/fallback-image.jpg'
+                      }
+                      unoptimized
+                    />
+                    <div className="col-span-1 text-center">
+                      {cardsData[offer.cardId].cardName}
+                    </div>
+                  </>
+                ) : (
+                  <>
+                    {/* 로딩 중일 때 또는 카드 데이터가 없을 때 */}
+                    <div className="col-span-1 text-center">Loading...</div>
+                  </>
+                )}
+                <div className="col-span-1 text-center">{offer.price}</div>
                 <div className="col-span-1 text-center">
-                  {offer.offeredPrice}
-                </div>
-                <div className="col-span-1 text-center">
-                  To: {offer.to} {/* 수신자 정보 유지 */}
+                  To: {offer.owner} {/* 수신자 정보 유지 */}
                 </div>
               </div>
             </div>
--- src/app/mypage/[id]/offers/page.tsx
+++ src/app/mypage/[id]/offers/page.tsx
@@ -4,6 +4,55 @@ import useUserIdStore from '@/store/useUserId';
 import React, { useEffect, useState } from 'react';
 import OffersList, { Offer } from './components/OfferList';
 
+export interface CardItem {
+  _id: string;
+  cardName: string;
+  price: {
+    lastPrice: number;
+    currentPrice: number;
+  };
+  image: string;
+  attributes: { background: string };
+} //카드 콜렉션이랑 똑같음
+
+const Page: React.FC = () => {
+  // const res = await CardApi.getAllCard();
+  // const res = await authApi.offers();
+  // const res = await authApi.status();
+  // console.log(res);
+  const userId = useUserIdStore((state) => state.userId);
+
+  const [offers, setOffers] = useState<Offer[]>([]); // API에서 받은 제안 데이터를 저장하는 상태
+
+  useEffect(() => {
+    const fetchOffers = async () => {
+      if (userId) {
+        try {
+          const res = await authApi.offers(userId);
+          // console.log('Fetched offers:', res); // 응답 데이터 확인
+          setOffers(res.offers); // 상태에 API에서 받은 데이터를 저장
+        } catch (error) {
+          console.error('Error fetching offers:', error);
+        }
+      } else {
+        console.log('userId is null or not set yet');
+      }
+    };
+
+    fetchOffers();
+  }, [userId]);
+  return (
+    <div className="p-8 bg-white shadow rounded-lg">
+      <h1 className="text-2xl font-bold mb-4">제안</h1>
+      <p>여기는 사용자가 제안한 아이템들을 보여주는 페이지입니다.</p>
+      <OffersList offers={offers} />
+    </div>
+  );
+};
+
+export default Page;
+/*
+
 const offersData: Offer[] = [
   {
     id: 1,
@@ -84,50 +133,4 @@ const offersData: Offer[] = [
     to: 'Tina',
   },
 ];
-export interface CardItem {
-  _id: string;
-  cardName: string;
-  price: {
-    lastPrice: number;
-    currentPrice: number;
-  };
-  image: string;
-  attributes: { background: string };
-} //카드 콜렉션이랑 똑같음
-
-const Page: React.FC = () => {
-  // const res = await CardApi.getAllCard();
-  // const res = await authApi.offers();
-  // const res = await authApi.status();
-  // console.log(res);
-  const userId = useUserIdStore((state) => state.userId);
-
-  const [offers, setOffers] = useState<Offer[]>([]); // API에서 받은 제안 데이터를 저장하는 상태
-
-  useEffect(() => {
-    const fetchOffers = async () => {
-      if (userId) {
-        try {
-          const res = await authApi.offers(userId);
-          console.log('Fetched offers:', res); // 응답 데이터 확인
-          setOffers(res); // 상태에 API에서 받은 데이터를 저장
-        } catch (error) {
-          console.error('Error fetching offers:', error);
-        }
-      } else {
-        console.log('userId is null or not set yet');
-      }
-    };
-
-    fetchOffers();
-  }, [userId]);
-  return (
-    <div className="p-8 bg-white shadow rounded-lg">
-      <h1 className="text-2xl font-bold mb-4">제안</h1>
-      <p>여기는 사용자가 제안한 아이템들을 보여주는 페이지입니다.</p>
-      <OffersList offers={offersData} />
-    </div>
-  );
-};
-
-export default Page;
+*/
