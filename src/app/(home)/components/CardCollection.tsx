3aa664d151c3a525b05ef579c00ec66c0cac274b
YoungSeungJang <1345620@naver.com>
2024-09-18 16:45:25 +0900

    :lipstick: CardList 반응형

--- src/app/(home)/components/CardCollection.tsx
+++ src/app/(home)/components/CardCollection.tsx
@@ -27,21 +27,21 @@ const CardCollection: React.FC<ICardCollectionProps> = ({
 }) => {
   return (
     <>
-      {viewMode === 'list' && (
-        <div className="h-fit w-full">
-          <ul className="flex font-semibold border-b-2 mt-4 p-2">
-            <li className="flex-[1.7]">아이템</li>
-            <li className="flex-[1]">현재 가격</li>
-            <li className="flex-[1]">마지막 판매</li>
-            <li className="flex-[1]">소유자</li>
-            <li className="flex-[1.2]">판매 기간</li>
-          </ul>
-          {card.map((item) => (
-            <CardList key={item._id} card={item} />
-          ))}
-        </div>
-      )}
-      <div className="flex flex-wrap">
+      <div className="w-full grid grid-cols-[repeat(auto-fit,_minmax(250px,_1fr))] gap-4 p-4 bg-red-400">
+        {viewMode === 'list' && (
+          <div className="h-fit w-full">
+            <ul className="flex font-semibold border-b-2 mt-4 p-2">
+              <li className="flex-[1.7]">아이템</li>
+              <li className="flex-[1]">현재 가격</li>
+              <li className="flex-[1]">마지막 판매</li>
+              <li className="flex-[1]">소유자</li>
+              <li className="flex-[1.2]">판매 기간</li>
+            </ul>
+            {card.map((item) => (
+              <CardList key={item._id} card={item} />
+            ))}
+          </div>
+        )}
         {card.map((item) => (
           <Card
             key={item._id}
